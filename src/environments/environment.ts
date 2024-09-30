aad98a318b8483c3c1f3a963394576242cfc62cd
Med khalil bousnina <78953896+khalilxg@users.noreply.github.com>
2024-10-01 00:03:49 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ek48o88c8ok4kw04sccoc04k.51.77.192.8.sslip.io:8989',
+  apiUrl: 'https://smgb.ghariani.com.tn',
   geminiToken: 'AIzaSyD9q23v2dor09E7zRrTZr26g0g9xhH1qGg',
 };
