d270a7123f625cb1f9f2f15745758712768ad328
Med khalil bousnina <78953896+khalilxg@users.noreply.github.com>
2024-10-01 02:55:59 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://0.0.0.0:8989/',
+  apiUrl: 'https://localhost:8989',
   geminiToken: 'AIzaSyD9q23v2dor09E7zRrTZr26g0g9xhH1qGg',
 };
