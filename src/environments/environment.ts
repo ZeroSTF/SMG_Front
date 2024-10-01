35fd5fcde9414be19a72257275fcd774e4cfcea5
Med khalil bousnina <78953896+khalilxg@users.noreply.github.com>
2024-10-01 02:42:04 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://smgb.ghariani.com.tn/',
+  apiUrl: 'http://0.0.0.0:8989/',
   geminiToken: 'AIzaSyD9q23v2dor09E7zRrTZr26g0g9xhH1qGg',
 };
